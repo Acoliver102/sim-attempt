0326fdb66fce4830218f8d03d8431ce45f884fd3
Acoliver102 <55566341+Acoliver102@users.noreply.github.com>
2021-02-03 17:31:21 -0600

    bupijln

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,9 +4,22 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.wpilibj.simulation.AnalogGyroSim;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
+import edu.wpi.first.wpilibj.simulation.EncoderSim;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.util.Units;
+import edu.wpi.first.wpiutil.math.VecBuilder;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -20,6 +33,48 @@ public class Robot extends TimedRobot {
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
+  private Field2d m_field = new Field2d();
+
+  private XboxController controller = new XboxController(0);
+
+  // These represent our regular encoder objects, which we would
+// create to use on a real robot.
+  private Encoder m_leftEncoder = new Encoder(0, 1);
+  private Encoder m_rightEncoder = new Encoder(2, 3);
+
+  // These are our EncoderSim objects, which we will only use in
+// simulation. However, you do not need to comment out these
+// declarations when you are deploying code to the roboRIO.
+  private EncoderSim m_leftEncoderSim = new EncoderSim(m_leftEncoder);
+  private EncoderSim m_rightEncoderSim = new EncoderSim(m_rightEncoder);
+
+  // Create our gyro object like we would on a real robot.
+  private AnalogGyro m_gyro = new AnalogGyro(1);
+
+  // Create the simulated gyro object, used for setting the gyro
+// angle. Like EncoderSim, this does not need to be commented out
+// when deploying code to the roboRIO.
+  private AnalogGyroSim m_gyroSim = new AnalogGyroSim(m_gyro);
+
+  DifferentialDriveOdometry m_odometry = new DifferentialDriveOdometry(
+          new Rotation2d(0.0) , new Pose2d(0.0, 0.0, new Rotation2d(0.0)));
+
+  // Create the simulation model of our drivetrain.
+  DifferentialDrivetrainSim m_driveSim = new DifferentialDrivetrainSim(
+          DCMotor.getNEO(2),       // 2 NEO motors on each side of the drivetrain.
+          7.29,                    // 7.29:1 gearing reduction.
+          7.5,                     // MOI of 7.5 kg m^2 (from CAD model).
+          60.0,                    // The mass of the robot is 60 kg.
+          Units.inchesToMeters(3), // The robot uses 3" radius wheels.
+          0.7112,                  // The track width is 0.7112 meters.
+
+          // The standard deviations for measurement noise:
+          // x and y:          0.001 m
+          // heading:          0.001 rad
+          // l and r velocity: 0.1   m/s
+          // l and r position: 0.005 m
+          VecBuilder.fill(0.001, 0.001, 0.001, 0.1, 0.1, 0.005, 0.005));
+
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
@@ -29,6 +84,9 @@ public class Robot extends TimedRobot {
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
+
+    SmartDashboard.putData("Field", m_field);
+
   }
 
   /**
@@ -95,4 +153,33 @@ public class Robot extends TimedRobot {
   /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
+
+  public void simulationPeriodic() {
+    // Set the inputs to the system. Note that we need to convert
+    // the [-1, 1] PWM signal to voltage by multiplying it by the
+    // robot controller voltage.
+    m_driveSim.setInputs(-12.0*controller.getRawAxis(1) + 6.0*controller.getRawAxis(4),
+            -12.0*controller.getRawAxis(1) - 6.0*controller.getRawAxis(4));
+
+    // Advance the model by 20 ms. Note that if you are running this
+    // subsystem in a separate thread or have changed the nominal timestep
+    // of TimedRobot, this value needs to match it.
+    m_driveSim.update(0.02);
+
+    // Update all of our sensors.
+    m_leftEncoderSim.setDistance(m_driveSim.getLeftPositionMeters());
+    m_leftEncoderSim.setRate(m_driveSim.getLeftVelocityMetersPerSecond());
+    m_rightEncoderSim.setDistance(m_driveSim.getRightPositionMeters());
+    m_rightEncoderSim.setRate(m_driveSim.getRightVelocityMetersPerSecond());
+    m_gyroSim.setAngle(-m_driveSim.getHeading().getDegrees());
+
+    m_odometry.update(m_gyro.getRotation2d(),
+            m_leftEncoder.getDistance(),
+            m_rightEncoder.getDistance());
+    m_field.setRobotPose(m_odometry.getPoseMeters());
+
+
+
+  }
+
 }
